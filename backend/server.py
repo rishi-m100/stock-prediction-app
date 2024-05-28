ea007963cc3378e6242dc6a808e0c5ae74b8fdfc
Rishi Mulchandani <rishimulchandani100@gmail.com>
2024-05-28 06:14:51 -0400

    endpoint fix

--- backend/server.py
+++ backend/server.py
@@ -24,7 +24,7 @@ def serve():
     return send_from_directory(app.static_folder, 'index.html')
 
 model = load_model('models/stock_predictor.h5')
-scaler = joblib.load('/Users/rishimulchandani/stock-prediction-ml-app/backend/models/scaler.pkl')
+scaler = joblib.load('models/scaler.pkl')
 
 @app.route("/input", methods=['POST'])
 def input():
